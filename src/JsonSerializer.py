bc1639b466f6ec7b1055158be99d9a1ac2712567
Alexander York <therealalexy@outlook.com>
2025-10-01 16:11:19 -0400

    finished early structure

---
+++ src/JsonSerializer.py
@@ -0,0 +1,11 @@
+# Class to create and test Json creation
+import json
+
+# Test data
+class TestPlayer:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Main stuff
