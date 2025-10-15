79086a7c0a9fb0eb3a15c5a86eed6a2fa4393cde
Alexander York <therealalexy@outlook.com>
2025-10-15 15:42:42 -0400

    Worked on testing some new data

--- src/JsonSerializer.py
+++ src/JsonSerializer.py
@@ -2,10 +2,31 @@
 import json
 
 # Test data
-class TestPlayer:
+class TestClass:
 
-    def __init__(self, name, age):
+    def __init__(self, name, hit_dice):
         self.name = name
-        self.age = age
+        self.hit_dice = hit_dice
+
+# Use the user to make some data
+print("Here is a test way to make some data!")
+while(True):                                        # NOT IMPLEMENTED YET
+    new_char = TestClass
+    print("Enter a class name:")
+    break
 
 # Main stuff
+filepath = 'test.json'
+newData = {
+    "ArtificerClass": {
+        "name": "Artificer", 
+        "hit dice": {
+            "number":1,
+            "faces":8
+            }
+        }
+    
+    }
+
+with open(filepath, "w") as file:
+    json.dump(newData, file, indent=4)
